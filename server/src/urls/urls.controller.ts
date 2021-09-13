052176a438313d99898f156d91887d6787ef67d5
Sven Kang <sven.c.kang@gmail.com>
2021-09-13 00:33:20 -0500

    feat: add actual log for url controller endpoint

--- server/src/urls/urls.controller.ts
+++ server/src/urls/urls.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { CreateHobbitLinkDto } from './create-url.dto';
 import { UrlsService, CreateHobbitResponse } from './urls.service';
 
 @Controller('urls')
 export class UrlsController {
-  constructor(private readonly urlsService: UrlsService) {}
+  constructor(
+    private readonly urlsService: UrlsService,
+    private readonly logger: Logger,
+  ) {
+    logger.setContext(this.constructor.name);
+  }
 
   @Post()
   postUrl(
     @Body() createHobbitLinkDto: CreateHobbitLinkDto,
   ): CreateHobbitResponse {
-    return this.urlsService.createHobbitLink(createHobbitLinkDto);
+    const startTime = new Date().getTime();
+    const response = this.urlsService.createHobbitLink(createHobbitLinkDto);
+    const endTime = new Date().getTime();
+    this.logger.log(
+      `Successfully created a hobbit link +${endTime - startTime}ms`,
+    );
+    return response;
   }
 }
--- server/src/urls/urls.service.ts
+++ server/src/urls/urls.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateHobbitLinkDto } from './create-url.dto';
 import { nanoid } from 'nanoid';
 
@@ -12,19 +12,9 @@ export interface CreateHobbitResponse {
 
 @Injectable()
 export class UrlsService {
-  constructor(private readonly logger: Logger) {
-    this.logger.setContext(this.constructor.name);
-  }
-
   createHobbitLink(
     createHobbitLinkDto: CreateHobbitLinkDto,
   ): CreateHobbitResponse {
-    this.logger.log(
-      `Attempting to create a hobbit link with ${JSON.stringify(
-        createHobbitLinkDto,
-      )}`,
-    );
-
     return {
       url: createHobbitLinkDto.url,
       hobbitLink:
