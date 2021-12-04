4f8c236195832ba7d0e3eed52620d673b42d2253
Sven Kang <sven.c.kang@gmail.com>
2021-12-03 21:40:16 -0600

    feat: initial swagger bootstrap setup

--- server/src/app/app.config.ts
+++ server/src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 import * as Joi from 'joi';
 
-const DEFAULT_PORT = 5050;
-const DEFAULT_NODE_ENV = 'development';
-
 export const configuration: ConfigModuleOptions = {
   isGlobal: true,
   validationSchema: Joi.object({
-    NODE_PORT: Joi.number().required().default(DEFAULT_PORT),
-    NODE_ENV: Joi.string().required().default(DEFAULT_NODE_ENV),
+    NODE_PORT: Joi.number().required(),
+    NODE_ENV: Joi.string().required(),
+    APP_NAME: Joi.string().required(),
+    APP_VER: Joi.string().required(),
   }),
   validationOptions: {
     allowUnknown: true,
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app/app.module';
@@ -14,8 +15,22 @@ async function bootstrap() {
     }),
   );
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('APP_PORT', { infer: true });
+  const port = configService.get<number>('NODE_PORT', { infer: true });
   const nodeEnv = configService.get<string>('NODE_ENV', { infer: true });
+  const appName = configService.get<string>('APP_NAME', { infer: true });
+  const appVersion = configService.get<string>('APP_VER', { infer: true });
+
+  if (nodeEnv !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle(appName)
+      .setDescription(`Welcome to ${appName} API documentation`)
+      .setVersion(appVersion)
+      .addTag(appName)
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(port);
   app
     .get(LoggerService)
